1adf6133487e99a366cb1e2084365e356f92ec37
vargajozsi <sikloernyos@hotmail.com>
2021-08-26 13:39:01 +0200

    javascript file attached - hamburgermenu repaired - submain menus deattached

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+//main hamburger menu hide begin//
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function(){
+  this.classList.toggle('close');
+}) 
+//main hamburger menu hide end//
