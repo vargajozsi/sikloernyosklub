517e5153b74051b365648a27c147615670b16398
József Varga <sikloernyos@hotmail.com>
2021-11-14 19:42:18 +0100

    club modul for script loading clubs

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
+import { clubs } from "./clubs.js";
 //main hamburger menu hide begin//
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', function(){
-  this.classList.toggle('close');
-}) 
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("close");
+});
 //main hamburger menu hide end//
+
+class clubFill {
+  constructor(klubnev, email, gps, tel, website, Klubvezeto) {
+    
+  }
+}
+
+const findClubWrapper = document.querySelector(".wrapper-klubs");
+findClubWrapper.appendChild(document.createElement('div')).innerText = 'goal';
